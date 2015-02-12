7c85907cdafbeacedbf440961f5c406209da3434
Christian Chandler <247webdev+github@googlemail.com>
2015-02-11 18:23:44 -0800

    First solution where delete only clears out data of the hash in the users array. Another commit will be refactor of the delete to remove the hash from the array.

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ users = []
 id = 1
 
 # routes to implement:
-#
+
 # GET / - show all users
 get '/' do
   @users = users
@@ -25,11 +25,47 @@ get '/' do
 end
 
 # GET /users/new - display a form for making a new user
-#
+get '/users/new' do
+  erb :new
+end
+
 # POST /users - create a user based on params from form
-#
+post '/users' do
+  @users = users
+
+  @users.push id: id, first: params[:first], last: params[:last], user_name: params[:user_name], pw: params[:pw]
+
+  id += 1
+  redirect to '/'
+end
+
 # GET /users/:id - show a user's info by their id, this should display the info in a form
-#
+get '/users/:id' do
+  # "Hello World"
+  @user = users[params[:id].to_i - 1 ]  
+  erb :edit
+end
+
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
-#
+put '/users/:id' do
+  @user = users[params[:id].to_i - 1 ]
+
+  @user[:first] = params[:first]
+  @user[:last] = params[:last]
+  @user[:user_name] = params[:user_name]
+  @user[:pw] = params[:pw]
+
+  redirect to '/'
+end
+
 # DELETE /users/:id - delete a user by their id
+delete '/users/:id' do
+  user = users[params[:id].to_i - 1 ]
+
+  user[:first] = ""
+  user[:last] = ""
+  user[:user_name] = ""
+  user[:pw] = ""
+
+  redirect to '/'
+end
